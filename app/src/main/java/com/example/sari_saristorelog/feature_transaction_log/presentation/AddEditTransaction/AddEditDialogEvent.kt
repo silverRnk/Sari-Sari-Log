2f8d5bd778fbd8f7973f81c71ea9a96800b47c5c
pdbautista0 <patdi.bautista@gmail.com>
2022-12-21 01:17:36 +0800

    Edit AddItemDialogEvents params type
    Edited due to change of params type on AddItemDialog State

--- app/src/main/java/com/example/sari_saristorelog/feature_transaction_log/presentation/AddEditTransaction/AddEditDialogEvent.kt
+++ app/src/main/java/com/example/sari_saristorelog/feature_transaction_log/presentation/AddEditTransaction/AddEditDialogEvent.kt
@@ -1,12 +1,11 @@
 package com.example.sari_saristorelog.feature_transaction_log.presentation.AddEditTransaction
 
-import androidx.compose.ui.text.input.TextFieldValue
 
 sealed class AddEditDialogEvent{
     data class OnDescriptionChange(val description: String): AddEditDialogEvent()
-    data class OnQuantityChange(val quantity: TextFieldValue): AddEditDialogEvent()
-    data class OnPriceChange(val price: TextFieldValue): AddEditDialogEvent()
-    data class OnSubtotalChange(val subtotal: TextFieldValue): AddEditDialogEvent()
+    data class OnQuantityChange(val quantity: String): AddEditDialogEvent()
+    data class OnPriceChange(val price: String): AddEditDialogEvent()
+    data class OnSubtotalChange(val subtotal: String): AddEditDialogEvent()
     object OnPositiveButton: AddEditDialogEvent()
     object OnCancel: AddEditDialogEvent()
 }
