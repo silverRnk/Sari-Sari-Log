5fe0881da56af1ce9ccf7c4b8744480c49152242
pdbautista0 <patdi.bautista@gmail.com>
2022-12-21 01:14:43 +0800

    Edit AddItemDialogState Params
    Edit type to from TextFieldValue to String:
     * quantity
     * price
     * subtotal
    Added params for highlighting invalid input on fields

--- app/src/main/java/com/example/sari_saristorelog/feature_transaction_log/presentation/AddEditTransaction/state/AddItemDialogState.kt
+++ app/src/main/java/com/example/sari_saristorelog/feature_transaction_log/presentation/AddEditTransaction/state/AddItemDialogState.kt
@@ -1,10 +1,10 @@
 package com.example.sari_saristorelog.feature_transaction_log.presentation.AddEditTransaction.state
 
-import androidx.compose.ui.text.input.TextFieldValue
-
 data class AddItemDialogState(
     val description: String = "",
-    val quantity: TextFieldValue = TextFieldValue(text = "0"),
-    val price: TextFieldValue = TextFieldValue(text = "0"),
-    val subtotal: TextFieldValue = TextFieldValue(text = "0")
+    val quantity: String = "1",
+    val isQuantityInvalidInput: Boolean = false,
+    val price: String = "",
+    val subtotal: String = "0",
+    val isSubtotalInputError: Boolean = false
 )
