b4e9513dfa71aab2a5787debdb11d31b802b8601
pdbautista0 <patdi.bautista@gmail.com>
2022-12-25 14:31:40 +0800

    Change CustomerInfoState
    remove add name field, add info field
    added for consitency

--- app/src/main/java/com/example/sari_saristorelog/feature_transaction_log/presentation/AddEditTransaction/state/CustomerInfoState.kt
+++ app/src/main/java/com/example/sari_saristorelog/feature_transaction_log/presentation/AddEditTransaction/state/CustomerInfoState.kt
@@ -2,9 +2,17 @@ package com.example.sari_saristorelog.feature_transaction_log.presentation.AddEd
 
 import androidx.annotation.DrawableRes
 import com.example.sari_saristorelog.R
+import com.example.sari_saristorelog.feature_transaction_log.domain.model.TransactionInfo
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
 
 data class CustomerInfoState(
-    val name: String = "",
-    @DrawableRes val customerIcon: Int = R.drawable.ic_baseline_face_24,
-    val isInputError: Boolean = false
+    val info: TransactionInfo = TransactionInfo(
+        customerName = "",
+        customerIcon = R.drawable.ic_baseline_face_24,
+        createdDate = LocalDateTime.of(LocalDate.now(), LocalTime.now()),
+        total = 0.0),
+    val isInputError: Boolean = false,
+    val isDateStatusVisible: Boolean = false
 )
