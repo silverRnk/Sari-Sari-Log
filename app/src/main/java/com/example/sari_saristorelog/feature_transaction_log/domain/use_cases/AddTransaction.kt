7ccf7a55fec4d3cca66d58a98f8ee39dfbcb3251
pdbautista0_01 <patdi.bautista@gmail.com>
2022-12-25 14:41:43 +0800

    Add Throwable Exception to AddTransaction Use Case

--- app/src/main/java/com/example/sari_saristorelog/feature_transaction_log/domain/use_cases/AddTransaction.kt
+++ app/src/main/java/com/example/sari_saristorelog/feature_transaction_log/domain/use_cases/AddTransaction.kt
@@ -1,5 +1,6 @@
 package com.example.sari_saristorelog.feature_transaction_log.domain.use_cases
 
+import com.example.sari_saristorelog.feature_transaction_log.domain.model.InvalidTransaction
 import com.example.sari_saristorelog.feature_transaction_log.domain.model.Items
 import com.example.sari_saristorelog.feature_transaction_log.domain.model.Transaction
 import com.example.sari_saristorelog.feature_transaction_log.domain.model.TransactionInfo
@@ -16,9 +17,13 @@ class AddTransaction(
     3. map the id to the item list
     4. add items to db
      */
+    @kotlin.jvm.Throws(InvalidTransaction::class)
     suspend operator fun invoke(info: TransactionInfo, items: List<Items>){
+        if(info.customerName.isEmpty()){
+            throw InvalidTransaction("Name field cannot be empty")
+        }else if(items.isEmpty()){
+            throw InvalidTransaction("List cannot be empty")
+        }
         repository.insertTransaction(info, items)
-
-
     }
 }
