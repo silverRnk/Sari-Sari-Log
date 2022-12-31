e497ddbcbafe793aa8ba098234f663476254ef19
pdbautista0 <patdi.bautista@gmail.com>
2022-12-31 16:11:24 +0800

    Added Confirm Transactin ViewModel and Screen Files

--- app/src/main/java/com/example/sari_saristorelog/feature_transaction_log/presentation/ConfirrmTransaction/ConfirmTransactionScreen.kt
+++ app/src/main/java/com/example/sari_saristorelog/feature_transaction_log/presentation/ConfirrmTransaction/ConfirmTransactionScreen.kt
@@ -1,2 +1,13 @@
 package com.example.sari_saristorelog.feature_transaction_log.presentation.ConfirrmTransaction
 
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+
+@Composable
+fun ConfirmTransactionScreen(
+    navController: NavController,
+    viewModel: ConfirmTransactionViewModel = hiltViewModel()
+){
+
+}
--- app/src/main/java/com/example/sari_saristorelog/feature_transaction_log/presentation/ConfirrmTransaction/ConfirmeTransactionViewModel.kt
+++ app/src/main/java/com/example/sari_saristorelog/feature_transaction_log/presentation/ConfirrmTransaction/ConfirmeTransactionViewModel.kt
@@ -1,4 +1,17 @@
 package com.example.sari_saristorelog.feature_transaction_log.presentation.ConfirrmTransaction
 
-class ConfirmeTransactionViewModel {
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.sari_saristorelog.feature_transaction_log.domain.use_cases.TransactionLogUseCases
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ConfirmTransactionViewModel @Inject constructor(
+    private val useCases: TransactionLogUseCases,
+    savedStateHandle: SavedStateHandle
+): ViewModel() {
+
+
+
 }
