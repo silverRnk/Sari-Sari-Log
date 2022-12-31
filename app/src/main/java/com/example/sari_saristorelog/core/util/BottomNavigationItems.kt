8b1e285ec5e375f3b0b95ec998f28b311dd9bd8f
pdbautista0 <patdi.bautista@gmail.com>
2022-12-31 15:33:04 +0800

    Add Route
    decouplued routes to BottomNavigationItems

--- app/src/main/java/com/example/sari_saristorelog/core/util/BottomNavigationItems.kt
+++ app/src/main/java/com/example/sari_saristorelog/core/util/BottomNavigationItems.kt
@@ -5,8 +5,8 @@ import androidx.annotation.StringRes
 import com.example.sari_saristorelog.R
 
 sealed class BottomNavigationItems(val route: String, @StringRes val label: Int, @DrawableRes val icon: Int){
-    object Home: BottomNavigationItems(route = "home", label = R.string.nav_item_1, icon = R.drawable.ic_home_24)
-    object Add: BottomNavigationItems(route = "add", label = R.string.nav_item_2, icon = R.drawable.ic_add_circle_outline_24)
+    object Home: BottomNavigationItems(route = Route.HOME_SCREEN, label = R.string.nav_item_1, icon = R.drawable.ic_home_24)
+    object Add: BottomNavigationItems(route = Route.ADD_EDIT_TRANSACTION_SCREEN, label = R.string.nav_item_2, icon = R.drawable.ic_add_circle_outline_24)
 
     companion object NavList{
         val navItems = listOf<BottomNavigationItems>(
---
+++ app/src/main/java/com/example/sari_saristorelog/core/util/Route.kt
@@ -0,0 +1,7 @@
+package com.example.sari_saristorelog.core.util
+
+object Route {
+    val HOME_SCREEN = "home_screen"
+    val ADD_EDIT_TRANSACTION_SCREEN = "add_edit_transaction_screen"
+    val CONFIRM_TRANSACTION_SCREEN = "confirm_transaction_screen"
+}
