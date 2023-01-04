dcdd934054a6cd823b06ffb7aad2cdf0e342d588
pdbautista0 <patdi.bautista@gmail.com>
2023-01-04 21:36:20 +0800

    added Logger Repository

--- app/src/main/java/com/example/sari_saristorelog/feature_transaction_log/data/repository/LoggerRepositoryImp.kt
+++ app/src/main/java/com/example/sari_saristorelog/feature_transaction_log/data/repository/LoggerRepositoryImp.kt
@@ -1,5 +1,6 @@
 package com.example.sari_saristorelog.feature_transaction_log.data.repository
 
+import android.util.Log
 import com.example.sari_saristorelog.core.data.repository.ApplicationDao
 import com.example.sari_saristorelog.feature_transaction_log.domain.model.Items
 import com.example.sari_saristorelog.feature_transaction_log.domain.model.Transaction
@@ -19,8 +20,10 @@ class LoggerRepositoryImp @Inject constructor(
     override suspend fun insertTransaction(info: TransactionInfo, items: List<Items>) {
         val transactionId = dao.insertTransactionInfo(info)
 
+        Log.d("Items", transactionId.toString())
         items.forEach { item -> item.transactionId = transactionId }
 
+        Log.d("Items", items.toString())
         dao.insertItems(items) }
 
     override suspend fun getTransaction(id: Long): Transaction? {
