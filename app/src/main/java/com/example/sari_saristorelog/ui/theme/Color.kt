de1341b01f85daecc5d8caf7e7c9cd90a75536ee
pdbautista0 <patdi.bautista@gmail.com>
2023-01-06 20:07:44 +0800

    Edited Theme Colors

--- app/src/main/java/com/example/sari_saristorelog/ui/theme/Color.kt
+++ app/src/main/java/com/example/sari_saristorelog/ui/theme/Color.kt
@@ -1,10 +1,15 @@
 package com.example.sari_saristorelog.ui.theme
 
-import androidx.compose.material.Colors
 import androidx.compose.ui.graphics.Color
 
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
-val Surface1 = Color(0xFFEEDEDE)
+val Surface1 = Color(0xFFF2F2F2)
+
+val Blue1 = Color(0xFF363EFF)
+val Green1 = Color(0xFF37FF3F)
+val Yellow1 = Color(0xFFFFEC37)
+val Orange1 = Color(0xFFFFA337)
+
